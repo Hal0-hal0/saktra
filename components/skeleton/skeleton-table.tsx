9f679070523d58402458de6dd47ffbfd3b70b666
jorich <dohinogjorich23@gmail.com>
2026-04-11 20:10:41 +0800

    admin
    feat: invite user, skeleton, delete user,a nd proxy update to inactive users

--- components/skeleton/skeleton-table.tsx
+++ components/skeleton/skeleton-table.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonTable() {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-2">
-      {Array.from({ length: 5 }).map((_, index) => (
+    <div className="flex w-full max-w flex-col gap-2">
+      {Array.from({ length: 10 }).map((_, index) => (
         <div className="flex gap-4" key={index}>
           <Skeleton className="h-4 flex-1" />
           <Skeleton className="h-4 w-24" />
